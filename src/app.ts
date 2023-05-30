77cf7a1ea0d853475ded48d9d56486a3c9ab6d42
FCroccoli <nandique@gmail.com>
2023-05-30 18:21:45 -0300

    feat: added login route and interfaces

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,13 @@ import "express-async-errors";
 import express from "express";
 import { errorHandler } from "./error/errors";
 import cors from "cors";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(userRoutes);
 
 app.use(errorHandler);
 
